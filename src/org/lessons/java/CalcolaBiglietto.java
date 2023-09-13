c681dee3cab7a046b853face0985bc3735909d0b
andreaconv <andrea2convertini@gmail.com>
2023-09-13 16:45:26 +0200

    [add] calculation

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -1,8 +1,30 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class CalcolaBiglietto {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		Scanner sc = new Scanner(System.in);
+		
+		//chiedo i KM
+		System.out.print("inserisci il numero di chilometri che vuoi percorrere");
+		int km = sc.nextInt();
+		
+		//chiedo l'età
+		System.out.print("inserisci il numero di anni che hai");
+		int age = sc.nextInt();
+		
+		//calcolo il costo del biglietto
+		double fullPrice = km * 0.21;
+		
+		if(age < 18 ) {
+			fullPrice = fullPrice - (fullPrice * 0.2);
+		} else if (age >= 65) {
+			fullPrice = fullPrice - (fullPrice * 0.4);
+		}
+		
+		System.out.println("prezzo del biglietto: " + fullPrice );
+		
 	}
 
 }
